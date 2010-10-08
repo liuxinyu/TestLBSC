cc037aea13e2521162773cc24c06bd55ca754a7e
Liu Xinyu <liuxy.cn@gmail.com>
2010-10-09 01:25:21 +0800

    add main view

--- gen/com/lxy/lbsc/R.java
+++ gen/com/lxy/lbsc/R.java
@@ -11,8 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int logobar=0x7f020001;
+        public static final int grid_me=0x7f020000;
+        public static final int grid_place=0x7f020001;
+        public static final int grid_question=0x7f020002;
+        public static final int grid_setting=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int logobar=0x7f020005;
     }
     public static final class id {
         public static final int maingrid=0x7f050000;
--- src/com/lxy/lbsc/LBSC.java
+++ src/com/lxy/lbsc/LBSC.java
@@ -5,13 +5,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.AdapterView; 
+import android.widget.Toast;
 
-public class LBSC extends Activity 
-	implements AdapterView.OnItemSelectedListener {
+public class LBSC extends Activity {
 	GridView menuGrid; 
 	String menu_items[]={"地点", "问题", "我", "设置" }; 
 	/** Called when the activity is first created. */
@@ -21,39 +24,50 @@ public class LBSC extends Activity
         setContentView(R.layout.main);
         
         menuGrid = (GridView) findViewById(R.id.maingrid);
-        menuGrid.setAdapter(new MaingridAdapter(this, 
-        		android.R.layout.simple_list_item_1, 
-        		menu_items)); 
-        // menuGrid.setOnItemSelectedListener(this); 
+        menuGrid.setAdapter(new ImageAdapter(this));
+        menuGrid.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(LBSC.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-    
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-    	// selection.setText(menu_items[position]);
-    	// which main item is selected
-    }
-        
-	public void onNothingSelected(AdapterView<?> arg0) {
-		// TODO Auto-generated method stub
-		
+	public class ImageAdapter extends BaseAdapter {
+	    private Context mContext;
+
+	    public ImageAdapter(Context c) {
+	        mContext = c;
+	    }
+	    public int getCount() {
+	        return mThumbIds.length;
+	    }
+	    public Object getItem(int position) {
+	        return null;
+	    }
+	    public long getItemId(int position) {
+	        return 0;
+	    }
+	    // create a new ImageView for each item referenced by the Adapter
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        ImageView imageView;
+	        if (convertView == null) {  // if it's not recycled, initialize some attributes
+	            imageView = new ImageView(mContext);
+	            imageView.setLayoutParams(new GridView.LayoutParams(160, 160));
+	            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+	            imageView.setPadding(10, 10, 10, 10);
+	        } else {
+	            imageView = (ImageView) convertView;
+	        }
+
+	        imageView.setImageResource(mThumbIds[position]);
+	        return imageView;
+	    }
+
+	    // references to our images
+	    private Integer[] mThumbIds = {
+	            R.drawable.grid_place, 
+	            R.drawable.grid_question,
+	            R.drawable.grid_me,
+	            R.drawable.grid_setting
+	    };
 	}
-	
-    private class MaingridAdapter extends ArrayAdapter<Object>{
-    	Context ctxt; 
-		public MaingridAdapter(Context context, int textViewResourceId,
-				String[] items) {
-			super(context, textViewResourceId, items);
-			// TODO Auto-generated constructor stub
-			this.ctxt = context;
-		}
-		
-		public View getview(int position, View convertView, ViewGroup parent){
-			TextView label = (TextView)convertView;
-			if (convertView==null){
-				convertView = new TextView (ctxt);
-				label = (TextView)convertView;
-			}
-			label.setText(menu_items[position]); 
-			return parent;			
-		}   	
-    }       
 }
