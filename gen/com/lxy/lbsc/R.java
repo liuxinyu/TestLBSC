ed22aa6a402977356dbb3f36d70215d3a16d9c42
Liu Xinyu <liuxy.cn@gmail.com>
2010-10-19 01:02:15 +0800

    'oct18'

--- gen/com/lxy/lbsc/R.java
+++ gen/com/lxy/lbsc/R.java
@@ -28,16 +28,19 @@ public final class R {
         public static final int tab_question=0x7f02000b;
     }
     public static final class id {
-        public static final int button_loginnow=0x7f060002;
-        public static final int button_question=0x7f060004;
-        public static final int button_signupnow=0x7f060001;
-        public static final int icon_place=0x7f060005;
-        public static final int maingrid=0x7f060003;
+        public static final int button_login=0x7f060003;
+        public static final int button_loginnow=0x7f060005;
+        public static final int button_question=0x7f060007;
+        public static final int button_signupnow=0x7f060004;
+        public static final int edit_email1=0x7f060001;
+        public static final int edit_password1=0x7f060002;
+        public static final int icon_place=0x7f060008;
+        public static final int maingrid=0x7f060006;
         public static final int place_header_name=0x7f060000;
-        public static final int place_name=0x7f060006;
-        public static final int place_number_of_access=0x7f060007;
-        public static final int place_number_of_questions=0x7f060008;
-        public static final int place_number_of_questions2=0x7f060009;
+        public static final int place_name=0x7f060009;
+        public static final int place_number_of_access=0x7f06000a;
+        public static final int place_number_of_questions=0x7f06000b;
+        public static final int place_number_of_questions2=0x7f06000c;
     }
     public static final class layout {
         public static final int listheader=0x7f030000;
@@ -48,6 +51,16 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int email=0x7f040002;
+        public static final int err_no_email=0x7f04000a;
+        public static final int err_no_password=0x7f04000b;
         public static final int hello=0x7f040000;
+        public static final int login=0x7f040007;
+        public static final int name=0x7f040004;
+        public static final int password=0x7f040003;
+        public static final int password_confirm=0x7f040006;
+        public static final int password_set=0x7f040005;
+        public static final int signup=0x7f040008;
+        public static final int url_login=0x7f040009;
     }
 }
--- src/com/lxy/lbsc/login_mgmt.java
+++ src/com/lxy/lbsc/login_mgmt.java
@@ -2,10 +2,17 @@
  * 
  */
 package com.lxy.lbsc;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ImageButton;
 
@@ -14,26 +21,50 @@ import android.widget.ImageButton;
  *
  */
 public class login_mgmt extends Activity {
+	private HttpClient client;
+	private String format;
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.login_mgmt);
-		Toast.makeText(login_mgmt.this, "From login_mgmt activity", Toast.LENGTH_SHORT).show();
+		format=getString(R.string.url_login);
 		
-		ImageButton signup = (ImageButton)findViewById(R.id.button_signupnow);
-		signup.setOnClickListener(new Button.OnClickListener(){  
+		Button btn_login = (Button)findViewById(R.id.button_login);
+		btn_login.setOnClickListener(new Button.OnClickListener(){  
             @Override  
             public void onClick(View v) {  
-            	Toast.makeText(login_mgmt.this, "TO sign up", Toast.LENGTH_SHORT).show();
-            }  
-        });  
-		
-		ImageButton login = (ImageButton)findViewById(R.id.button_loginnow);
-		login.setOnClickListener(new Button.OnClickListener(){  
-            @Override  
-            public void onClick(View v) {  
-            	Toast.makeText(login_mgmt.this, "TO login", Toast.LENGTH_SHORT).show();
+            	login();
             }  
         }); 
+	}
+	
+	private void login(){
+		EditText edit = (EditText)findViewById(R.id.edit_email1); 
+		String email = edit.getText().toString(); 
+		if(email.length()==0){
+			Toast.makeText(login_mgmt.this,getString(R.string.err_no_email) , Toast.LENGTH_SHORT).show();
+			return;
+		}
+		edit = (EditText)findViewById(R.id.edit_password1);
+		String password = edit.getText().toString(); 
+		if(password.length()==0){
+			Toast.makeText(login_mgmt.this,getString(R.string.err_no_password) , Toast.LENGTH_SHORT).show();
+			return;
+		}
+		
+		String url=String.format(format, email, password);
+		HttpGet getMethod=new HttpGet(url);
+		try {
+			ResponseHandler<String> responseHandler=new BasicResponseHandler();
+			String responseBody=client.execute(getMethod, responseHandler);
+			// to handle return result from JSON
+			//buildForecasts(responseBody);			
+		}
+		catch (Throwable t) {
+			android.util.Log.e("WeatherDemo", "Exception fetching data", t);
+			Toast
+				.makeText(this, "Request failed: "+t.toString(), 4000)
+				.show();
+		}
 	}
 }
